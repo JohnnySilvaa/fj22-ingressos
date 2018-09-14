47ef05873a19ec2dd26e7bafd844dbe3a1a7f0bd
Johnny Silvaa <johnnysilva_estagio@hotmail.com>
2018-09-14 14:31:50 -0300

    Desabilitando a seleção do lugar que já está no carrinho

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -16,6 +16,10 @@ public class Carrinho {
 	     public void add(Ingresso ingresso){
 	         ingressos.add(ingresso);
 	     }
+	     
+	     public boolean isSelecionado(Lugar lugar){
+	         return ingressos.stream().map(Ingresso::getLugar).anyMatch(l -> l.equals(lugar));
+	     }
 
 
 }
